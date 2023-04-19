7c9368c44c168087cb602fd1d9cc8bb768773141
Shellyda Barbosa <shellyda.barbosa@ufpe.br>
2023-04-19 15:05:07 -0300

    update: creating new insert for tables

--- Projeto Físico/POVOAMENTO.sql
+++ Projeto Físico/POVOAMENTO.sql
@@ -1,27 +1,14 @@
 -- POVOAMENTO DE CLIENTES
 
-INSERT INTO CLIENTE VALUES ('12345678910', 'João', '12345678', '1234');
-INSERT INTO CLIENTE VALUES ('12345678911', 'Maria', '12345677', '1235');
-INSERT INTO CLIENTE VALUES ('12345678912', 'Luana', '12345678', '1435');
-INSERT INTO CLIENTE VALUES ('12345678913', 'Raul', '22345678', '2234');
-INSERT INTO CLIENTE VALUES ('12345678914', 'Ana', '22345677', '2235');
-INSERT INTO CLIENTE VALUES ('12345678915', 'William', '22345678', '2435');
-INSERT INTO CLIENTE VALUES ('12345678916', 'Felipe', '32345678', '3234');
-INSERT INTO CLIENTE VALUES ('12345678917', 'Henrique', '32345677', '3235');
-INSERT INTO CLIENTE VALUES ('12345678918', 'Viviane', '32345678', '3435');
-
--- POVOAMENTO DE TELEFONES
-
-INSERT INTO TELEFONE VALUES ('12345678910', '999999999');
-INSERT INTO TELEFONE VALUES ('12345678911', '999999998');
-INSERT INTO TELEFONE VALUES ('12345678912', '999999997');
-INSERT INTO TELEFONE VALUES ('12345678913', '999999996');
-INSERT INTO TELEFONE VALUES ('12345678914', '999999995');
-INSERT INTO TELEFONE VALUES ('12345678915', '999999994');
-INSERT INTO TELEFONE VALUES ('12345678916', '999999993');
-INSERT INTO TELEFONE VALUES ('12345678917', '999999992');
-INSERT INTO TELEFONE VALUES ('12345678918', '999999991');
-
+INSERT INTO CLIENTE VALUES ('859.839.606-01', 'Tânia Dias Oliveira', tp_endereco('24743-690', 'Rua Augusto Melo', '89', 'Recife'), tp_telefones(tp_telefone('(19)78456-2145'), tp_telefone('(19)78456-2146'), tp_telefone('(19)78456-2147')));
+INSERT INTO CLIENTE VALUES ('752.789.603-02', 'João da Silva Santos', tp_endereco('11013-330', 'Rua Conselheiro Rodrigues Alves', '135', 'Santos'), tp_telefones(tp_telefone('(13)1234-5678'), tp_telefone('(13)9876-5432')));
+INSERT INTO CLIENTE VALUES ('359.257.401-05', 'Luana Sousa Oliveira', tp_endereco('88025-001', 'Rua João Pio Duarte Silva', '234', 'Florianópolis'), tp_telefones(tp_telefone('(48)3214-5678'), tp_telefone('(48)9876-4321')));
+INSERT INTO CLIENTE VALUES ('548.176.809-10', 'Pedro Henrique Rocha', tp_endereco('31030-270', 'Rua Diamantina', '42', 'Belo Horizonte'), tp_telefones(tp_telefone('(31)3245-6789'), tp_telefone('(31)9876-5432'), tp_telefone('(31)9215-6478')));
+INSERT INTO CLIENTE VALUES ('951.802.207-07', 'Mariana Lima Santos', tp_endereco('60170-051', 'Rua Osvaldo Cruz', '67', 'Fortaleza'), tp_telefones(tp_telefone('(85)3874-1234'), tp_telefone('(85)9867-4321')));
+INSERT INTO CLIENTE VALUES ('114.987.023-15', 'Fernando Silva Carvalho', tp_endereco('40170-010', 'Avenida Antônio Carlos Magalhães', '157', 'Salvador'), tp_telefones(tp_telefone('(71)3267-8901'), tp_telefone('(71)9876-5432')));
+INSERT INTO CLIENTE VALUES ('123.456.789-00', 'Ana Luíza Oliveira', tp_endereco('40310-010', 'Rua Castro Alves', '123', 'Salvador'), tp_telefones(tp_telefone('(71)9999-8888'), tp_telefone('(71)5555-4444')));
+INSERT INTO CLIENTE VALUES ('789.654.321-99', 'Marcos Pereira', tp_endereco('01310-100', 'Avenida Paulista', '456', 'São Paulo'), tp_telefones(tp_telefone('(11)7777-2222')));
+INSERT INTO CLIENTE VALUES ('111.222.333-44', 'Juliana Almeida', tp_endereco('80060-240', 'Rua José Bonifácio', '789', 'Curitiba'), tp_telefones(tp_telefone('(41)1111-2222'), tp_telefone('(41)3333-4444'), tp_telefone('(41)5555-6666')));
 
 -- POVOAMENTO DE ANIMAIS
 
@@ -38,10 +25,15 @@ INSERT INTO ANIMAL VALUES ('A000000009', '12345678918', 'Rex', 'Dálmata', 'M', 
 
 -- POVOAMENTO DE CLINICAS 
 
-INSERT INTO CLINICA VALUES ('12345678901234', 'Clinica Veterinária São Francisco', 'Rua São Francisco, 123 - Centro');
-INSERT INTO CLINICA VALUES ('23456789012345', 'Clínica Veterinária Amigo Fiel', 'Av. das Palmeiras, 567 - Jardim Botânico');
-INSERT INTO CLINICA VALUES ('34567890123456', 'Pet Clínica Animais de Estimação', 'Rua dos Animais, 789 - Vila Animal');
-INSERT INTO CLINICA VALUES ('45678901234567', 'Veterinária PetShop', 'Av. das Flores, 321 - Jardim das Flores');
+INSERT INTO CLINICA VALUES ('12345678901234', 'Teste', tp_endereco('54743-690', 'R4a Augusto Melo', '89', 'Recife'));
+INSERT INTO CLINICA VALUES ('12345678901235', 'Clínica Popular', tp_endereco('88010-410', 'Rua da Praia', '456', 'Florianópolis'));
+INSERT INTO CLINICA VALUES ('12345678901236', 'Clínica da Família', tp_endereco('30110-000', 'Avenida das Flores', '789', 'Belo Horizonte'));
+INSERT INTO CLINICA VALUES ('12345678901237', 'Clínica São Francisco', tp_endereco('13020-150', 'Rua das Palmeiras', '321', 'Campinas'));
+INSERT INTO CLINICA VALUES ('12345678901238', 'Clínica Vida Saudável', tp_endereco('04050-001', 'Rua dos Girassóis', '555', 'São Paulo'));
+INSERT INTO CLINICA VALUES ('12345678901239', 'Clínica do Coração', tp_endereco('58013-040', 'Avenida Central', '777', 'João Pessoa'));
+INSERT INTO CLINICA VALUES ('12345678901240', 'Clínica Bem Estar', tp_endereco('79020-210', 'Rua dos Ipês', '222', 'Campo Grande'));
+INSERT INTO CLINICA VALUES ('12345678901241', 'Clínica Sorriso Perfeito', tp_endereco('22290-240', 'Avenida da Praia', '123', 'Rio de Janeiro'));
+INSERT INTO CLINICA VALUES ('12345678901243', 'Clínica da felicidade', tp_endereco('15000-000', 'Avenida das Magnólias', '789', 'São José do Rio Preto'));
 
 
 -- POVOAMENTO DE DESCONTOS
@@ -58,7 +50,6 @@ INSERT INTO DESCONTO VALUES ('DESC50', 50.00);
 
 -- POVAMENTO DE CONTRATAM
 
-
 INSERT INTO CONTRATAM VALUES ('12345678910', '12345678901234', 'DESC10');
 INSERT INTO CONTRATAM VALUES ('12345678911', '23456789012345', 'DESC15');
 INSERT INTO CONTRATAM VALUES ('12345678912', '34567890123456', 'DESC20');
@@ -71,15 +62,15 @@ INSERT INTO CONTRATAM VALUES ('12345678918', '12345678901234', 'DESC30');
 
 -- POVOAMENTO DE EMPREGADOS
 
-INSERT INTO EMPREGADO VALUES ('E00001', '12345678901234', '1112345678', 'João', 2500.00, NULL);
-INSERT INTO EMPREGADO VALUES ('E00002', '23456789012345', '1223456789', 'Maria Santos', 3500.00, 'E00001');
-INSERT INTO EMPREGADO VALUES ('E00003', '34567890123456', '2234567890', 'Pedro Souza', 2200.00, NULL);
-INSERT INTO EMPREGADO VALUES ('E00004', '45678901234567', '3245678901', 'Ana Costa', 2800.00, 'E00001');
-INSERT INTO EMPREGADO VALUES ('E00005', '45678901234567', '1456789012', 'Lucas Lima', 4200.00, 'E00003');
-INSERT INTO EMPREGADO VALUES ('E00006', '34567890123456', '8267890123', 'Júlia Oliveira', 3000.00, 'E00002');
-INSERT INTO EMPREGADO VALUES ('E00007', '34567890123456', '8478901234', 'Márcia Mendes', 3600.00, 'E00001');
-INSERT INTO EMPREGADO VALUES ('E00008', '12345678901234', '8189012345', 'Fernando Santos', 4000.00, NULL);
-INSERT INTO EMPREGADO VALUES ('E00009', '12345678901234', '5290123456', 'Thiago Rodrigues', 5000.00, 'E00008');
+INSERT INTO EMPREGADO VALUES ('Ana', '98765432101', tp_endereco('04828-010', 'Rua Carlos Caldeira Filho', '45', 'São Paulo'), '98765432101123', 3500, tp_telefones(tp_telefone('(11) 94567-8912')), NULL);
+INSERT INTO EMPREGADO VALUES ('Pedro', '12345678902', tp_endereco('04108-041', 'Rua Américo Brasiliense', '320', 'São Paulo'), '12345678902123', 2800, tp_telefones(tp_telefone('(11) 98567-1234')), NULL);
+INSERT INTO EMPREGADO VALUES ('Maria', '34567891234', tp_endereco('30140-080', 'Rua dos Guajajaras', '1000', 'Belo Horizonte'), '34567891234123', 4000, tp_telefones(tp_telefone('(31) 98765-4321')), NULL);
+INSERT INTO EMPREGADO VALUES ('Luiz', '23456789102', tp_endereco('88010-300', 'Rua dos Ilhéus', '85', 'Florianópolis'), '23456789102123', 3200, tp_telefones(tp_telefone('(48) 98876-5432')), NULL);
+INSERT INTO EMPREGADO VALUES ('Lorena', '09876543210', tp_endereco('04567-123', 'Rua Augusta', '450', 'São Paulo'), '09876543210123', 3500.00, tp_telefones(tp_telefone('(11) 94567-8912')), (SELECT REF(E) FROM EMPREGADO E WHERE CPF = '12345678912'));
+INSERT INTO EMPREGADO VALUES ('Rafael', '23456789101', tp_endereco('30110-021', 'Rua da Bahia', '1000', 'Belo Horizonte'), '23456789101123', 2800.00, tp_telefones(tp_telefone('(31) 98567-1234')), (SELECT REF(E) FROM EMPREGADO E WHERE CPF = '12345678913'));
+INSERT INTO EMPREGADO VALUES ('Carla', '34567891235', tp_endereco('88010-301', 'Avenida Beira Mar', '150', 'Florianópolis'), '34567891235123', 4000.00, tp_telefones(tp_telefone('(48) 98876-5432')), (SELECT REF(E) FROM EMPREGADO E WHERE CPF = '12345678914'));
+INSERT INTO EMPREGADO VALUES ('Renato', '45678912302', tp_endereco('22220-030', 'Rua Visconde de Pirajá', '500', 'Rio de Janeiro'), '45678912302123', 3200.00, tp_telefones(tp_telefone('(21) 99765-4321')), (SELECT REF(E) FROM EMPREGADO E WHERE CPF = '12345678915'));
+INSERT INTO EMPREGADO VALUES ('Gabriela', '56789123406', tp_endereco('04108-042', 'Rua Américo Brasiliense', '250', 'São Paulo'), '56789123406123', 5000.00, tp_telefones(tp_telefone('(11) 98876-5432')), (SELECT REF(E) FROM EMPREGADO E WHERE CPF = '12345678916'));
 
 -- POVOAMENTO DE GRADUADO
 
@@ -99,15 +90,15 @@ INSERT INTO TECNICO VALUES ('E00005', 'Técnico de Nutrição');
 
 -- POVOAMENTO DE LOJA
 
-INSERT INTO LOJA VALUES ('L000000001', '12345678901234', '1111111111', 'Rua das Flores, 123 - Centro');
-INSERT INTO LOJA VALUES ('L000000002', '23456789012345', '2222222222', 'Av. dos Girassóis, 456 - Jardim Botânico');
-INSERT INTO LOJA VALUES ('L000000003', '34567890123456', '3333333333', 'Rua dos Animais, 789 - Vila Animal');
-INSERT INTO LOJA VALUES ('L000000004', '45678901234567', '4444444444', 'Av. dos Passarinhos, 321 - Jardim das Flores');
-INSERT INTO LOJA VALUES ('L000000005', '45678901234567', '5555555555', 'Rua dos Gatos, 555 - Vila dos Animais');
-INSERT INTO LOJA VALUES ('L000000006', '12345678901234', '6666666666', 'Av. dos Cachorros, 987 - Vila dos Animais');
-INSERT INTO LOJA VALUES ('L000000007', '45678901234567', '7777777777', 'Rua dos Pássaros, 111 - Vila das Aves');
-INSERT INTO LOJA VALUES ('L000000008', '45678901234567', '8888888888', 'Av. dos Cavalos, 222 - Jardim dos Animais');
-INSERT INTO LOJA VALUES ('L000000009', '34567890123456', '9999999999', 'Rua dos Hamsters, 444 - Vila dos Animais');
+INSERT INTO LOJA VALUES ('L000000002', '34567890123456', tp_telefones(tp_telefone('(21)98765-4321')), tp_endereco('22031-072', 'Rua Vinícius de Moraes', '12', 'Rio de Janeiro'));
+INSERT INTO LOJA VALUES ('L000000003', '56789012345678', tp_telefones(tp_telefone('(11)98567-1234')), tp_endereco('04005-005', 'Rua Peixoto Gomide', '707', 'São Paulo'));
+INSERT INTO LOJA VALUES ('L000000004', '89012345678901', tp_telefones(tp_telefone('(81)98765-4321')), tp_endereco('50050-130', 'Rua do Sol', '300', 'Recife'));
+INSERT INTO LOJA VALUES ('L000000005', '23456789012345', tp_telefones(tp_telefone('(31)98876-5432')), tp_endereco('30130-110', 'Rua dos Carijós', '150', 'Belo Horizonte'));
+INSERT INTO LOJA VALUES ('L000000006', '45678901234567', tp_telefones(tp_telefone('(48)98876-5432')), tp_endereco('88062-001', 'Rodovia SC 401', '3730', 'Florianópolis'));
+INSERT INTO LOJA VALUES ('L000000007', '67890123456789', tp_telefones(tp_telefone('(21)99765-4321')), tp_endereco('22430-060', 'Rua Dias Ferreira', '247', 'Rio de Janeiro'));
+INSERT INTO LOJA VALUES ('L000000008', '90123456789012', tp_telefones(tp_telefone('(11)94567-8912')), tp_endereco('04581-000', 'Avenida Juscelino Kubitschek', '2041', 'São Paulo'));
+INSERT INTO LOJA VALUES ('L000000009', '23456789012345', tp_telefones(tp_telefone('(81)98876-5432')), tp_endereco('51030-000', 'Avenida Boa Viagem', '1200', 'Recife'));
+INSERT INTO LOJA VALUES ('L000000010', '45678901234567', tp_telefones(tp_telefone('(31)98567-1234')), tp_endereco('30330-000', 'Rua Pium-Í', '1420', 'Belo Horizonte'));
 
 -- POVOAMENTO DE SERVICO
 
